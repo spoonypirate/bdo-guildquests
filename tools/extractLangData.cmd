ed77952feda036c1009139b9f5aeeb3a25c94f30
Mike Masterson <mikeadm@lyxdesic.net>
2017-06-10 16:19:45 -0500

    initial commit, v452

---
+++ tools/extractLangData.cmd
@@ -0,0 +1,5 @@
+@echo off
+quickbms.exe -f "*languagedata_en.txt" -o -Y blackdesert.bms ..\PAD05388.PAZ .\Extracted\
+quickbms.exe -f "*languagedata_de.txt" -o -Y blackdesert.bms ..\PAD03774.PAZ .\Extracted\
+quickbms.exe -f "*languagedata_fr.txt" -o -Y blackdesert.bms ..\PAD05389.PAZ .\Extracted\
+
